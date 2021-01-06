b015025f3473f5e64dc7f70c9ec75e7640a6df61
jhlee <leejh8372@gmail.com>
2021-01-06 15:52:27 +0900

    add health check

--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+from models import Post
 
 # Create your tests here.
+class BlogTests(TestCase):
+    def setUp(self):
+        # Setup run before every test method.
+        pass
+
+    def tearDown(self):
+        # Clean up run after every test method.
+        pass
+
+    def test_health_check(self):
+        response = self.client.get(reverse('blog:index'))
+        self.assertEqual(response.status_code, 200)
+
