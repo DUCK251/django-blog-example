750c37fdd514b775471002ba936d04ebb6c15596
jhlee <leejh8372@gmail.com>
2021-01-06 14:37:59 +0900

    add blank line at the end of file

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ['DATABASE_NAME'],
         'USER': os.environ['DATABASE_USER'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD'], 
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
         'HOST': 'localhost',
         'PORT': '',
     }
